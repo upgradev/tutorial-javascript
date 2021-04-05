69e0d2d4895ada9c9e2977abc545c62f88c44f92
Cleison França Amorim <>
2021-04-05 14:07:02 -0400

    estudos

---
+++ 10_arrays/js/scripts.js
@@ -0,0 +1,14 @@
+var arr = [5, "joao", true, { teste: 1, teste2: 2 }];
+console.log(arr);
+
+var arr2 = [2, 3, 4, 5];
+console.log(arr2);
+
+console.log(arr[1]);
+console.log(arr2[0]);
+
+arr[4] = 10;
+console.log(arr);
+
+arr[0] = 3;
+console.log(arr);
